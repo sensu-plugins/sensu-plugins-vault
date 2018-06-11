bc1fe31a6f950be35ef4e6dd0730e5801536e5e3
Lee Briggs <lbriggs@apptio.com>
2018-06-11 11:22:57 -0700

    Add vault status check

---
+++ check-vault-status.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+#
+# check-vault-status
+# 
+# DESCRIPTION:
+#   This plugins checks if vault is up & reachable. It then checks
+#   if the vault is unsealed
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: vault
+#
+require 'sensu-plugin/check/cli'
+require 'vault'
+
+class VaultStatus < Sensu::Plugin::Check::CLI
+
+  option :host,
+    description: 'vault host',
+    short: '-h HOST',
+    long:  '--host HOST',
+    default: '127.0.0.1'
+
+
+  option :port,
+    description: 'vault port',
+    short: '-p PORT',
+    long: '--port PORT',
+    default: '8200'
+
+  option :scheme,
+    description: 'vault http scheme',
+    short: '-s SCHEME',
+    long: '--scheme SCHEME',
+    default: 'https'
+
+  option :insecure,
+    description: 'Allow insecure connections',
+    short: '-k',
+    boolean: true,
+    default: false
+
+  option :timeout,
+    description: 'Timeout on connections to vault',
+    short: '-t TIMEOUT',
+    long: '--timeout TIMEOUT',
+    default: 10
+
+  option :ssl_cert,
+    description: 'Path to PEM encoded SSL cert',
+    long: '--ssl-cert CERT'
+
+
+  def run
+
+    # set the vault address
+    Vault.address = "#{config[:scheme]}://#{config[:host]}:#{config[:port]}"
+
+    # Allow insecure SSL
+    Vault.ssl_verify = false if config[:insecure]
+
+    # Set the connection timeout
+    Vault.timeout = config[:timeout]
+
+    # Set SSL pem cert
+    Vault.ssl_ca_cert = config[:ssl_cert]
+
+    begin
+      if Vault.sys.seal_status.sealed? == true
+        critical "Vault is up, but it's sealed"
+      else
+        ok "Vault is up and unsealed"
+      end
+    rescue => ex
+       critical ex
+    end
+
+  end
+
+end
